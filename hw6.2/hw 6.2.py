0c10d56eaa176f59f6bed49b4f03214887e900cd
Dijoy1866 <113097359+Dijoy1866@users.noreply.github.com>
2022-09-23 11:42:34 +0300

    6.0

---
+++ hw6.2/hw 6.2.py
@@ -0,0 +1,19 @@
+# Вывести треугольник #2 с шириной N с помощью цикла while
+
+N = int(input("Please input number :"))
+
+num_height = 1
+
+
+while num_height <= N:
+    num_width = 1
+    while num_width <= num_height:
+        print("*", end=" ")
+        num_width += 1
+    num_height += 1
+    print()
+
+
+
+
+
---
+++ hw6.3/hw 6.3.py
@@ -0,0 +1,12 @@
+# Вывести треугольник #3 с шириной N с помощью цикла while
+
+N = int(input("Please input :"))
+i = N
+d = '*'
+s = N
+p = ' '
+print(d * i)
+while s != 1:
+    s -= 1
+    i -= 1
+    print(p * (N-i) + (d * i))
